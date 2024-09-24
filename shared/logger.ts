e2f1a988f5281f79bb8f242d3f23eec5155e80f0
Laureline <laurelinedev@gmail.com>
2024-09-24 19:55:44 +0200

    [ FILE SYSTEM ] - init file system x composite - nodemon watching files - build map [ build for frontend/backend/shared ]

---
+++ shared/logger.ts
@@ -0,0 +1,11 @@
+export const log = (content: string = 'hey', options: { type: 'ITER' | 'INFO' | 'MESSAGE' | 'ERROR' } = { type: 'MESSAGE' }) => {
+	const _log = {
+		'MESSAGE': console.log,
+		'INFO': console.info,
+		'ITER': console.table,
+		'ERROR': console.error
+	}
+	_log[options.type](content)
+}
+log('plop')
+
